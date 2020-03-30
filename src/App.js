882c2fd43a802b7f2b0c35dbde7342194b819125
gokmen.yilmaz <gokmen1977@gmail.com>
2020-03-30 11:13:20 +0300

    uu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import { BrowserRouter, Switch,Route, Link,NavLink } from "react-router-dom";
+import DashBoard from './DashBoard'
+import Home from './Home'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App  extends React.Component{
+
+  constructor(props)
+  {
+    super(props);
+  }
+
+  render()
+  {
+    return (
+
+      <section>
+
+        <div>
+          App Başlık
+        </div>
+
+        <div style={{border:'solid', borderColor:'red', margin:'10px'}}>
+          <nav>Dashboard</nav>
+          <DashBoard/>
+        </div>
+
+     
+        <article style={{border:'solid', borderColor:'green', margin:'10px'}}>
+          <div>Router içi</div>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/home" component={Home}/>
+              <Route exact path="/dashboard" component={DashBoard}/>
+            </Switch>
+          </BrowserRouter>
+        </article>
+      
+      
+      </section>
+      
+
+     );
+  }
+
 }
 
-export default App;
